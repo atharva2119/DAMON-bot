75c1f4f34df49bd82584a938d074937d91805ded
atharva2119 <117750591+atharva2119@users.noreply.github.com>
2025-09-28 11:28:56 +0530

    Add files via upload

---
+++ Frontend/GUI.py
@@ -0,0 +1,143 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QStackedWidget, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QFrame, QLabel
+from PyQt5.QtGui import QIcon, QMovie, QColor, QTextCharFormat, QPixmap
+from PyQt5.QtCore import Qt, QSize, QTimer
+from dotenv import dotenv_values
+import sys
+import os
+
+# Load environment variables
+env_vars = dotenv_values(".env")
+Assistantname = env_vars.get("Assistantname")  # Default to "Jarvis" if not found
+current_dir = os.getcwd()
+old_chat_message = ""
+TempDirPath = os.path.join(current_dir, "Frontend", "Files")
+GraphicsDirPath = os.path.join(current_dir, "Frontend", "Graphics")
+
+# Ensure required directories exist
+os.makedirs(TempDirPath, exist_ok=True)
+os.makedirs(GraphicsDirPath, exist_ok=True)
+
+def TempDirectoryPath(Filename):
+    return os.path.join(TempDirPath, Filename)
+
+def GraphicsDirectoryPath(Filename):
+    return os.path.join(GraphicsDirPath, Filename)
+
+def GetAssistantStatus():
+    try:
+        with open(TempDirectoryPath('Status.data'), "r", encoding='utf-8') as file:
+            return file.read()
+    except FileNotFoundError:
+        return ""
+
+class ChatSection(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout(self)
+        self.chat_text_edit = QTextEdit()
+        self.chat_text_edit.setReadOnly(True)
+        self.chat_text_edit.setStyleSheet("background-color: black; color: white; font-size: 14px;")
+        layout.addWidget(self.chat_text_edit)
+        
+        self.gif_label = QLabel()
+        self.gif_label.setAlignment(Qt.AlignRight | Qt.AlignBottom)
+        movie = QMovie(GraphicsDirectoryPath('Jarvis.gif'))
+        if movie.isValid():
+            movie.setScaledSize(QSize(480, 270))
+            self.gif_label.setMovie(movie)
+            movie.start()
+        layout.addWidget(self.gif_label)
+        
+        self.label = QLabel("")
+        self.label.setStyleSheet("color: white; font-size: 16px;")
+        self.label.setAlignment(Qt.AlignRight)
+        layout.addWidget(self.label)
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.updateStatus)
+        self.timer.start(100)
+    
+    def updateStatus(self):
+        self.label.setText(GetAssistantStatus())
+
+class InitialScreen(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        layout = QVBoxLayout(self)
+        self.gif_label = QLabel()
+        self.gif_label.setAlignment(Qt.AlignCenter)
+        movie = QMovie(GraphicsDirectoryPath('Jarvis.gif'))
+        if movie.isValid():
+            movie.setScaledSize(QSize(800, 450))
+            self.gif_label.setMovie(movie)
+            movie.start()
+        layout.addWidget(self.gif_label)
+        
+        self.label = QLabel("")
+        self.label.setStyleSheet("color: white; font-size: 16px;")
+        self.label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.label)
+        
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.updateStatus)
+        self.timer.start(100)
+    
+    def updateStatus(self):
+        self.label.setText(GetAssistantStatus())
+
+class MessageScreen(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        layout = QVBoxLayout(self)
+        self.chat_section = ChatSection()
+        layout.addWidget(self.chat_section)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setGeometry(100, 100, 800, 600)
+        self.setStyleSheet("background-color: black;")
+        
+        self.stacked_widget = QStackedWidget(self)
+        self.initial_screen = InitialScreen()
+        self.message_screen = MessageScreen()
+        self.stacked_widget.addWidget(self.initial_screen)
+        self.stacked_widget.addWidget(self.message_screen)
+        self.setCentralWidget(self.stacked_widget)
+        
+        self.top_bar = CustomTopBar(self, self.stacked_widget)
+        self.setMenuWidget(self.top_bar)
+
+class CustomTopBar(QWidget):
+    def __init__(self, parent, stacked_widget):
+        super().__init__(parent)
+        self.stacked_widget = stacked_widget
+        self.setFixedHeight(50)
+        layout = QHBoxLayout(self)
+        
+        home_button = QPushButton("Home")
+        home_button.setIcon(QIcon(GraphicsDirectoryPath("Home.png")))
+        home_button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        layout.addWidget(home_button)
+        
+        chat_button = QPushButton("Chat")
+        chat_button.setIcon(QIcon(GraphicsDirectoryPath("Chats.png")))
+        chat_button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(1))
+        layout.addWidget(chat_button)
+        
+        close_button = QPushButton()
+        close_button.setIcon(QIcon(GraphicsDirectoryPath("Close.png")))
+        close_button.clicked.connect(parent.close)
+        layout.addWidget(close_button)
+
+        self.setLayout(layout)
+
+def GraphicalUserInterface():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    GraphicalUserInterface()
